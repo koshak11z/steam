d40520f08f1163d01dedaa128543aa31e6d46c7c
koshak11z <166287330+koshak11z@users.noreply.github.com>
2024-04-09 21:42:56 +0300

    Update steam
    Убрал классы переместил всё в массив тоесть убрал хардкор. Блогодоря массиву код вместо 90 строчек стал в 60

--- main.py
+++ main.py
@@ -1,63 +1,17 @@
 import time
 
-login = '1'
-password = '1'
+# Создание массива
+# [username, password, balance, games, friends]
+user1 = ["1", "1", 1000, {"Counter-Strike 2": 15, "Among Us": 1, "GTA V": 100}, []]
 
-# Хранение
-class SteamUser:
-    def __init__(self, username, password, balance):
-        self.username = username
-        self.password = password
-        self.balance = balance
-        self.games = {}
-        self.friends = []
-
-    def login(self, username, password):
-        if self.username == username and self.password == password:
-            return True
-        else:
-            return False
-
-    def buy_game(self, game, price):
-        if price <= self.balance:
-            self.games[game] = price
-            self.balance -= price
-            print("Поздравляю! Вы купили игру", game)
-            print("Остаток на счете:", self.balance)
-        else:
-            print("У вас недостаточно средств для покупки игры", game)
-
-    def add_friend(self, friend):
-        self.friends.append(friend)
-        print("Вы добавили друга", friend)
-
-    def show_games(self):
-        print("\nСписок игр в магазине:")
-        for game, price in self.games.items():
-            print(game, "- $", price)
-
-    def show_friends(self):
-        print("\nСписок ваших друзей:")
-        for friend in self.friends:
-            print(friend)
-
-    def view_balance(self):
-        print("Ваш текущий баланс: $", self.balance)
-
-# Создание инфы о пользователе
-user1 = SteamUser("1", "1", 1000)
-user1.games = {
-    "Counter-Strike 2": 15,
-    "Among Us": 1,
-    "GTA V": 100
-}
-
-# Создание логина + пароля (конечно сложно, но по другому не получалось)
+# Ввод логина и пароля
 username_input = input("Введите ваш логин: ")
 password_input = input("Введите ваш пароль: ")
 
-if user1.login(username_input, password_input): 
+# Проверка логина и пароля
+if user1[0] == username_input and user1[1] == password_input:
     print("Вы успешно вошли в систему!")
+    
     # Функционал
     while True:
         print("\nДоступные команды:")
@@ -71,29 +25,44 @@ if user1.login(username_input, password_input):
         choice = input("Введите номер команды: ")
 
         if choice == '1':
-            user1.show_games()
+            print("\nСписок игр в магазине:")
+            for game, price in user1[3].items():
+                print(game, "- $", price)
+                
         elif choice == '2':
             game_choice = input("Введите название игры, которую хотите купить: ")
-            if game_choice in user1.games:
-                user1.buy_game(game_choice, user1.games[game_choice])
+            if game_choice in user1[3]:
+                if user1[2] >= user1[3][game_choice]:
+                    user1[2] -= user1[3][game_choice]
+                    print("Поздравляю! Вы купили игру", game_choice)
+                    print("Остаток на счете:", user1[2])
+                else:
+                    print("У вас недостаточно средств для покупки игры", game_choice)
             else:
                 print("Данной игры нет в магазине.")
+                
         elif choice == '3':
             friend_name = input("Введите имя друга: ")
-            user1.add_friend(friend_name)
+            user1[4].append(friend_name)
+            print("Вы добавили друга", friend_name)
+            
         elif choice == '4':
-            user1.show_friends()
+            print("\nСписок ваших друзей:")
+            for friend in user1[4]:
+                print(friend)
+                
         elif choice == '5':
-            user1.view_balance()
+            print("Ваш текущий баланс: $", user1[2])
+            
         elif choice == '6':
             print("До новых встреч в Steam!")
-            time.sleep (1)
+            time.sleep(1)
             break
+            
         else:
             print("Неверная команда. Повторите ввод.")
-else: 
+else:
     print("Ошибка входа. Пожалуйста, проверьте логин и пароль.")
-    time.sleep (1)
-    exit()                                                                                                                                                  
- 
- # 1
+    time.sleep(1)
+    exit()
+                                     
